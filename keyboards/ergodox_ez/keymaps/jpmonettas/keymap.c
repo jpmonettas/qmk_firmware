c50e60c2fc208d595e4263c3fb7d504dbe2a8684
Juan Monetta <jpmonettas@gmail.com>
2018-07-06 16:43:10 -0300

    Add a delete key

--- keyboards/ergodox_ez/keymaps/jpmonettas/keymap.c
+++ keyboards/ergodox_ez/keymaps/jpmonettas/keymap.c
@@ -93,7 +93,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *                                        |      |      |       |      |      |
  *                                 ,------|------|------|       |------+------+------.
  *                                 |      |      |      |       |      |      |      |
- *                                 |      |      |------|       |------|      |      |
+ *                                 |      |      |------|       |------|      | DEL  |
  *                                 |      |      |      |       |      |      |      |
  *                                 `--------------------'       `--------------------'
  */
@@ -111,12 +111,12 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
        // right hand
        KC_TRNS, KC_F6,   KC_F7,  KC_F8,   KC_F9,   KC_F10,  KC_F11,
        KC_TRNS, KC_TRNS,  KC_TRNS,  KC_UP,   KC_TRNS,   KC_TRNS, KC_F12,
-       KC_RPRN, KC_TRNS,  KC_DOWN,   KC_RIGHT,   KC_TRNS, KC_TRNS,
+       KC_TRNS, KC_LEFT,  KC_DOWN,   KC_RIGHT,   KC_TRNS, KC_TRNS,
        KC_TRNS, KC_TRNS, KC_TRNS,   KC_DOWN,    KC_TRNS,    KC_TRNS, KC_TRNS,
                          KC_TRNS,KC_TRNS,  KC_TRNS,   KC_TRNS,  KC_TRNS,
        KC_TRNS, KC_TRNS,
        KC_TRNS,
-       KC_TRNS, KC_TRNS, KC_TRNS
+       KC_TRNS, KC_TRNS, KC_DELETE
 ),
 /* Keymap 2: Media and mouse keys
  *
